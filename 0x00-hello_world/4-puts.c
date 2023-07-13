41a1fbb9b491b8ea092b1e5b2cad869a0e408a07
TS-mfon <mfonante8@gmail.com>
2023-07-13 14:07:16 +0100

    the commits

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
 /**
- *main - main entry point
- *return - always 0 {success}
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
