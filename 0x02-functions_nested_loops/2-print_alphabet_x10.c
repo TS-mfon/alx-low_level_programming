ea466acfee72afc50a917fffa6a3ae351adc4c90
TS-mfon <mfonante8@gmail.com>
2023-07-18 15:47:14 +0100

    Ten times better

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (j = 1; i <= 10; i++)
+	{
+		for (i = 97; i <= 122; i++)
+		{
+	  		_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
