f374157df8200ee92a01f5f7cf791a1163338933
TS-mfon <mfonante8@gmail.com>
2023-07-18 16:56:20 +0100

    Ten times better than one

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,22 +1,22 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
+ * _abs - computes the absolute value
+ * of an iterger
  *
- * Return: Always 0.
+ * @i: input number as an interger.
+ *
+ * Return: absolute value
  */
-int main(void)
-{
-	int r;
 
-	r = _abs(-1);
-	printf("%d\n", r);
-	r = _abs(0);
-	printf("%d\n", r);
-	r = _abs(1);
-	printf("%d\n", r);
-	r = _abs(-98);
-	printf("%d\n", r);
-	return (0);
+int _abs(int);
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -2);
+	}
 }
