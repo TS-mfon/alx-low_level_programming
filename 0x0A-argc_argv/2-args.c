465f37c7fba872cc5380c2342ff828a8479728e3
TS-mfon <mfonante8@gmail.com>
2023-08-05 09:49:13 +0100

    ARGC AND ARGV

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - prints the number of arguments passed
- * @argd: int
- * @argv: list
- * Return: Always 0
+ * main - function that prints all arguments it receives
+ * @argc: the size of the argv array, the number of command line arguments
+ * @argv: an array containing the program command line arguments
+ * Return: 0 if successful
  */
-int main(int argc, char const *argv[])
+
+int main(int argc, char *argv[])
 {
-	int i = 0;
+	int i;
 
-	while (argc--)
-	{
+	for (i = 0; i < argc; ++i)
 		printf("%s\n", argv[i]);
-		i++;
-	}
-
 	return (0);
 }
